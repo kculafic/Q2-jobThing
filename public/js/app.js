022a6ae176337ea752809716edcac31cf9335c4e
kculafic <kculafic@gmail.com>
2016-10-16 17:04:36 -0700

    Modals and User page added

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
   // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  'use strict';
+
   $('.modal-trigger').leanModal();
+
+
+
+  
 });
