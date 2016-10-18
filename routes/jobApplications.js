0b5f0ee37bc3ac8dfa92adb44490eb8ce9314807
kculafic <kculafic@gmail.com>
2016-10-18 11:24:27 -0700

    forgot to branch

--- routes/jobApplications.js
+++ routes/jobApplications.js
@@ -7,11 +7,28 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const knex = require('../knex');
 const { camelizeKeys, decamelizeKeys } = require('humps');
+
 const router = express.Router();
 const rp = require('request-promise');
 
-router.post('/jobApplications', /* AUTHORIZE, */ (req, res, next ) => {
+const authorize = function(req, res, next) {
+  jwt.verify(req.cookies.token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return next(boom.create(401, 'Unauthorized'));
+    }
+
+    req.token = decoded;
+    // You can now access the payload via req.token.userId
+    next();
+  });
+};
+
+
+
+router.post('/jobApplications', authorize, (req, res, next ) => {
   console.log(req.body);
+
+  
 
   const { name } = req.body;
 
